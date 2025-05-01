76d303c263e490a1308134a5b7023377ff63eaa7
Isa Bright <imbright@uwm.edu>
2025-05-01 17:08:38 -0500

    added main list

--- src/Main.java
+++ src/Main.java
@@ -54,13 +54,59 @@ public class Main {
             ArrayList<String> word39 = new ArrayList<>();
             ArrayList<String> word40= new ArrayList<>();
 
+            // add all lists to main list
+            ArrayList<ArrayList> lists = new ArrayList<>();
+            lists.add(null);
+            lists.add(word1);
+            lists.add(word2);
+            lists.add(word3);
+            lists.add(word4);
+            lists.add(word5);
+            lists.add(word6);
+            lists.add(word7);
+            lists.add(word8);
+            lists.add(word9);
+            lists.add(word10);
+            lists.add(word11);
+            lists.add(word12);
+            lists.add(word13);
+            lists.add(word14);
+            lists.add(word15);
+            lists.add(word16);
+            lists.add(word17);
+            lists.add(word18);
+            lists.add(word19);
+            lists.add(word20);
+            lists.add(word21);
+            lists.add(word22);
+            lists.add(word23);
+            lists.add(word24);
+            lists.add(word25);
+            lists.add(word26);
+            lists.add(word27);
+            lists.add(word28);
+            lists.add(word29);
+            lists.add(word30);
+            lists.add(word31);
+            lists.add(word32);
+            lists.add(word33);
+            lists.add(word34);
+            lists.add(word35);
+            lists.add(word36);
+            lists.add(word37);
+            lists.add(word38);
+            lists.add(word39);
+            lists.add(word40);
+
+
+
             int list = 0;
             String data;
 
             // populate arrays with words from file
             while (wordReader.hasNext()){
 
-                data = wordReader.next();
+                data = wordReader.nextLine();
                 if (data.isEmpty()){
                     continue;
                 }
@@ -68,87 +114,88 @@ public class Main {
                     list = Integer.parseInt(data);
                     continue;
                 }
+                lists.get(list).add(data);
 
-                if (list == 1)
-                    word1.add(data);
-                if (list == 2)
-                    word2.add(data);
-                if (list == 3)
-                    word3.add(data);
-                if (list == 4)
-                    word4.add(data);
-                if (list == 5)
-                    word5.add(data);
-                if (list == 6)
-                    word6.add(data);
-                if (list == 7)
-                    word7.add(data);
-                if (list == 8)
-                    word8.add(data);
-                if (list == 9)
-                    word9.add(data);
-                if (list == 10)
-                    word10.add(data);
-                if (list == 11)
-                    word11.add(data);
-                if (list == 12)
-                    word12.add(data);
-                if (list == 13)
-                    word13.add(data);
-                if (list == 14)
-                    word14.add(data);
-                if (list == 15)
-                    word15.add(data);
-                if (list == 16)
-                    word16.add(data);
-                if (list == 17)
-                    word17.add(data);
-                if (list == 18)
-                    word18.add(data);
-                if (list == 19)
-                    word19.add(data);
-                if (list == 20)
-                    word20.add(data);
-                if (list == 21)
-                    word21.add(data);
-                if (list == 22)
-                    word22.add(data);
-                if (list == 23)
-                    word23.add(data);
-                if (list == 24)
-                    word24.add(data);
-                if (list == 25)
-                    word25.add(data);
-                if (list == 26)
-                    word26.add(data);
-                if (list == 27)
-                    word27.add(data);
-                if (list == 28)
-                    word28.add(data);
-                if (list == 29)
-                    word29.add(data);
-                if (list == 30)
-                    word30.add(data);
-                if (list == 31)
-                    word31.add(data);
-                if (list == 32)
-                    word32.add(data);
-                if (list == 33)
-                    word33.add(data);
-                if (list == 34)
-                    word34.add(data);
-                if (list == 35)
-                    word35.add(data);
-                if (list == 36)
-                    word36.add(data);
-                if (list == 37)
-                    word37.add(data);
-                if (list == 38)
-                    word38.add(data);
-                if (list == 39)
-                    word39.add(data);
-                if (list == 40)
-                    word40.add(data);
+//                if (list == 1)
+//                    word1.add(data);
+//                if (list == 2)
+//                    word2.add(data);
+//                if (list == 3)
+//                    word3.add(data);
+//                if (list == 4)
+//                    word4.add(data);
+//                if (list == 5)
+//                    word5.add(data);
+//                if (list == 6)
+//                    word6.add(data);
+//                if (list == 7)
+//                    word7.add(data);
+//                if (list == 8)
+//                    word8.add(data);
+//                if (list == 9)
+//                    word9.add(data);
+//                if (list == 10)
+//                    word10.add(data);
+//                if (list == 11)
+//                    word11.add(data);
+//                if (list == 12)
+//                    word12.add(data);
+//                if (list == 13)
+//                    word13.add(data);
+//                if (list == 14)
+//                    word14.add(data);
+//                if (list == 15)
+//                    word15.add(data);
+//                if (list == 16)
+//                    word16.add(data);
+//                if (list == 17)
+//                    word17.add(data);
+//                if (list == 18)
+//                    word18.add(data);
+//                if (list == 19)
+//                    word19.add(data);
+//                if (list == 20)
+//                    word20.add(data);
+//                if (list == 21)
+//                    word21.add(data);
+//                if (list == 22)
+//                    word22.add(data);
+//                if (list == 23)
+//                    word23.add(data);
+//                if (list == 24)
+//                    word24.add(data);
+//                if (list == 25)
+//                    word25.add(data);
+//                if (list == 26)
+//                    word26.add(data);
+//                if (list == 27)
+//                    word27.add(data);
+//                if (list == 28)
+//                    word28.add(data);
+//                if (list == 29)
+//                    word29.add(data);
+//                if (list == 30)
+//                    word30.add(data);
+//                if (list == 31)
+//                    word31.add(data);
+//                if (list == 32)
+//                    word32.add(data);
+//                if (list == 33)
+//                    word33.add(data);
+//                if (list == 34)
+//                    word34.add(data);
+//                if (list == 35)
+//                    word35.add(data);
+//                if (list == 36)
+//                    word36.add(data);
+//                if (list == 37)
+//                    word37.add(data);
+//                if (list == 38)
+//                    word38.add(data);
+//                if (list == 39)
+//                    word39.add(data);
+//                if (list == 40)
+//                    word40.add(data);
             } // end While
 
 
